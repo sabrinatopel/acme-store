2fc517bc0e12b59914832c22caf41e97f32dee3b
Sabrina Topel <sabrina.f.topel@gmail.com>
2023-07-04 10:33:30 -0300

    save

--- acme-store/client-service/src/main/java/br/com/acme/adapters/output/database/h2/service/GetClientDomainByIdRepositoryService.java
+++ acme-store/client-service/src/main/java/br/com/acme/adapters/output/database/h2/service/GetClientDomainByIdRepositoryService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class GetClientByIDDomainRepositoryService implements IGetClientDomainByIdRepository {
+public class GetClientDomainByIdRepositoryService implements IGetClientDomainByIdRepository {
 
     private final ClientRepository clientRepository;
     private final ConverterDTO converterDTO;
